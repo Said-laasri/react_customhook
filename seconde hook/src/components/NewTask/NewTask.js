67210e802be535941f68f04d2ed8b42290d88a37
Said-laasri <saidlaasri3@gmail.com>
2023-03-29 16:16:22 +0000

    Update the newTask to use the Request POST method to send task to firebase

--- seconde hook/src/components/NewTask/NewTask.js
+++ seconde hook/src/components/NewTask/NewTask.js
@@ -1,44 +1,30 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createdTask = async (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+    console.log(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://task-6b94c-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  sendTaskRequest({
+    url: "https://task-6b94c-default-rtdb.firebaseio.com/tasks.json",
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+    method: "POST",
+    body: JSON.stringify({ text: taskText }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }, createdTask.bind(null, taskText));
 
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  
-    
-  };
+       };
 
   return (
     <Section>
