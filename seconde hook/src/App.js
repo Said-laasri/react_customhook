b57b00e1c34970dd289d9e60133a3f6ddea9f24c
Said-laasri <saidlaasri3@gmail.com>
2023-03-29 16:15:33 +0000

    Update the Appjs to use the custom hook for the Request Get mothod

--- seconde hook/src/App.js
+++ seconde hook/src/App.js
@@ -1,43 +1,31 @@
-import React, { useEffect, useState } from 'react';
-
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import React, { useEffect, useState } from "react";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://task-6b94c-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  useEffect(() => {
+    const transformTask = (data) => {
       const loadedTasks = [];
 
       for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+        // const item = JSON.parse(data[taskKey]);
+        loadedTasks.push({ id: taskKey, text: JSON.parse(data[taskKey]).text });
       }
-
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+      
+    };
+    fetchTasks(
+      {
+        url: "https://task-6b94c-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTask
+    );
+  },[fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
